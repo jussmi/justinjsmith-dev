f8610c3ed4d5366956c039fe32280567c3e36b2e
Justin Smith <smithjj42@gmail.com>
2021-05-04 20:00:20 +0000

    Adding publishing criteria to index

--- src/freeze.py
+++ src/freeze.py
@@ -1,6 +1,6 @@
 from flask_frozen import Freezer
 from app import app
-from app import pages
+from app import pages, post_is_active
 
 freezer = Freezer(app)
 
@@ -8,7 +8,8 @@ freezer = Freezer(app)
 @freezer.register_generator
 def entries():
     for page in pages:
-        yield {"path": page.path}
+        if post_is_active(page):
+            yield {"path": page.path}
 
 
 if __name__ == "__main__":
--- src/pages/index.md
+++ src/pages/index.md
@@ -1,3 +1,4 @@
 title: Justin J. Smith's Site
 description: A place for me to put down some thoughts and practice some techniques.
 publish-datetime: 2021-04-25 12:00:00
+publish: true
